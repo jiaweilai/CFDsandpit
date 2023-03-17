a74cd8f9e5d22470455fa921f8602d21673702fb
jiaweilai <napoleonlai@hotmail.com>
2023-03-17 22:35:49 +0000

    main: add initial commit on pyhuo

---
+++ pyhuo/main.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+nt = 100
+
+if __name__ == '__main__':
+
+    # Data initialisation
+    # Record initial condition
+    # Time step loop
+
+    # Main loop
+    for n in range(nt+1):
+        # Implicit Runge-Kutta method
+
+    # Terminate the program
